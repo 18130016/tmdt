8129b67fa91f372ba5f44997411631d379772613
18130016 <74467162+18130016@users.noreply.github.com>
2022-06-08 14:28:55 +0700

    cmit

--- tmdt-agl/src/app/models/listproduct.ts
+++
@@ -1,6 +0,0 @@
-import {Product} from "./product";
-import dataproduct from '../data/product.json';
-import {CartItem} from "./cart-item";
-export var listProducts:Product[] = dataproduct;
-
-export var listCartItem:CartItem[]=[];
--- tmdt-agl/src/app/services/fillter/status-order.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatusOrderPipe } from './status-order.pipe';
-
-describe('StatusOrderPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatusOrderPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
